2146d5cdb011a9c720e49b9b2b4bba1ba850461f
Aionmagan <noelrock9@gmail.com>
2023-07-28 20:10:50 -0400

    ui Neysha

---
+++ AvenaEngine/gameplay/ui.c
@@ -0,0 +1,21 @@
+#include "ui.h"
+
+void ui_init()
+{
+
+}
+ 
+void ui_start()
+{
+
+}
+
+void ui_update()
+{
+
+}
+ 
+obj_t* ui_render()
+{
+	
+}
---
+++ AvenaEngine/gameplay/ui.h
@@ -0,0 +1,11 @@
+#ifndef __AUI_H__
+#define __AUI_H__
+
+#include "../fm/obj.h"
+
+void ui_init(); 
+void ui_start(); 
+void ui_update(); 
+obj_t* ui_render();
+	
+#endif
